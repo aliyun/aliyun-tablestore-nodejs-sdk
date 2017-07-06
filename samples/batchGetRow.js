b51d9d71e5a603b19878d3c2e7eebb496896755c
伯箫 <boxiao.wpl@alibaba-inc.com>
2017-07-06 16:57:29 +0800

    add retry code

--- samples/batchGetRow.js
+++ samples/batchGetRow.js
@@ -6,8 +6,8 @@ var params = {
     tables: [{
         tableName: 'sampleTable',
         primaryKey: [
-            [{ 'gid': Long.fromNumber(20008) }, { 'uid': Long.fromNumber(20008) }],
-            [{ 'gid': Long.fromNumber(20007) }, { 'uid': Long.fromNumber(20007) }]
+            [{ 'gid': Long.fromNumber(20013) }, { 'uid': Long.fromNumber(20013) }],
+            [{ 'gid': Long.fromNumber(20015) }, { 'uid': Long.fromNumber(20015) }]
         ],
         startColumn: "col2",
         endColumn: "col4"
@@ -21,10 +21,52 @@ var params = {
     ],
 };
 
-client.batchGetRow(params, function (err, data) {
-    if (err) {
-        console.log('error:', err);
-        return;
-    }
-    console.log('success:', data);
-});
+var maxRetryTimes = 3;
+var retryCount = 0;
+
+function batchGetRow(params) {
+    client.batchGetRow(params, function (err, data) {
+        if (err) {
+            console.log('error:', err);
+            return;
+        }
+
+        var isAllSuccess = true;
+        var retryRequest = { tables: [] };
+        for (var i = 0; i < data.tables.length; i++) {
+            var faildRequest = { tableName: data.tables[i][0].tableName, primaryKey: [] };
+
+            for (var j = 0; j < data.tables[i].length; j++) {
+                if (!data.tables[i][j].isOk && null != data.tables[i][j].primaryKey) {
+                    isAllSuccess = false;
+                    var pks = [];
+                    for (var k in data.tables[i][j].primaryKey) {
+                        var name = data.tables[i][j].primaryKey[k].name;
+                        var value = data.tables[i][j].primaryKey[k].value;
+                        var kp = {};
+                        kp[name] = value;
+                        pks.push(kp);
+                    }
+                    faildRequest.primaryKey.push(pks);
+
+                } else {
+                    // get success data
+                }
+            }
+
+            if (faildRequest.primaryKey.length > 0) {
+                retryRequest.tables.push(faildRequest);
+            }
+        }
+
+        if (!isAllSuccess && retryCount++ < maxRetryTimes) {
+            batchGetRow(retryRequest);
+        }
+
+        console.log('success:', data);
+    });
+}
+
+batchGetRow(params, maxRetryTimes);
+
+
