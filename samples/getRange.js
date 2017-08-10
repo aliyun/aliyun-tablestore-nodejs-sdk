d06725d0b76d759a6b7afa27a868364265a2e59d
伯箫 <boxiao.wpl@alibaba-inc.com>
2017-08-10 17:44:01 +0800

    add colums to get condition in get

--- samples/getRange.js
+++ samples/getRange.js
@@ -7,19 +7,30 @@ var params = {
   direction: TableStore.Direction.FORWARD,
   inclusiveStartPrimaryKey: [{ "gid": TableStore.INF_MIN }, { "uid": TableStore.INF_MIN }],
   exclusiveEndPrimaryKey: [{ "gid": TableStore.INF_MAX }, { "uid": TableStore.INF_MAX }],
-  limit: 50
+  limit: 2
 };
 
-client.getRange(params, function (err, data) {
-  if (err) {
-    console.log('error:', err);
-    return;
-  }
+var resultRows = []
 
-  //如果data.next_start_primary_key不为空，说明需要继续读取
-  if (data.next_start_primary_key) {
+var getRange = function () {
+  client.getRange(params, function (err, data) {
+    if (err) {
+      console.log('error:', err);
+      return;
+    }
+    resultRows = resultRows.concat(data.rows)
 
-  }
+    //如果data.next_start_primary_key不为空，说明需要继续读取
+    if (data.next_start_primary_key) {
+      params.inclusiveStartPrimaryKey = [
+        { "gid": data.next_start_primary_key[0].value },
+        { "uid": data.next_start_primary_key[1].value }
+      ]
+      getRange()
+    } else {
+      console.log(resultRows)
+    }
+  });
+}
 
-  console.log('success:', data);
-});
+getRange()
