7105aaacb83c89a7cb189d85a95b4740091c9d07
伯箫 <boxiao.wpl@alibaba-inc.com>
2017-08-15 22:43:58 +0800

    remove uncaughtException state and modify the relevant code

---
+++ samples/domainError.js
@@ -0,0 +1,19 @@
+var client = require('./client');
+var domain = require('domain')
+
+//https://nodejs.org/api/domain.html#domain_domain_enter
+//https://shapeshed.com/uncaught-exceptions-in-node/
+domainSample = domain.create();
+domainSample.on('error', function (err) {
+    console.log('error:', err);
+    console.log('Exit your application!')
+})
+domainSample.run(function () {
+    client.listTable({}, function (err, data) {
+        if (err) {
+            console.log('error:', err);
+            return;
+        }
+        throw new Error('test')
+    });
+})
