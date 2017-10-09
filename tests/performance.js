ea1fc23b4bd39e79e0d6eda4fac19f4721c3fad9
boxiao <boxiao.wpl@foxmail.com>
2017-10-09 03:55:41 -0500

    Update performance.js

--- tests/performance.js
+++ tests/performance.js
@@ -1,6 +1,8 @@
 var client = require('../samples/client');
 
-var roundTimes = 10000;
+// 测试的时候可以将这个值调大
+var testTimes = 1
+var roundTimes = testTimes;
 var completeTimes = 0;
 
 var start = new Date().getTime();
@@ -8,7 +10,7 @@ var start = new Date().getTime();
 while (roundTimes > 0) {
     client.listTable({}, function (err, data) {
         completeTimes++;
-        if (completeTimes >= 10000) {
+        if (completeTimes >= testTimes) {
             var end = new Date().getTime();
             completeTime = end - start;
             console.log('completeTime:' + completeTime);
