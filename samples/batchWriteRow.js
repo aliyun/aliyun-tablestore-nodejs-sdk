765b127ece4320923e9424e7e13ffae09daa5076
伯箫 <boxiao.wpl@alibaba-inc.com>
2017-11-06 09:52:09 +0800

    add PUT sample code

--- samples/batchWriteRow.js
+++ samples/batchWriteRow.js
@@ -3,20 +3,34 @@ var TableStore = require('../index.js');
 var Long = TableStore.Long;
 
 var params = {
-    tables: [{
-        tableName: 'sampleTable',
-        rows: [{
-            type: 'PUT',
-            condition: new TableStore.Condition(TableStore.RowExistenceExpectation.IGNORE, null),
-            primaryKey: [{ 'gid': Long.fromNumber(8) }, { 'uid': Long.fromNumber(80) }],
-            attributeColumns: [{ 'attrCol1': 'test1' }, { 'attrCol2': 'test2' }],
-            returnContent: { returnType: TableStore.ReturnType.Primarykey }
-        }],
-    }],
+
+    tables: [
+        {
+            tableName: 'sampleTable',
+            rows:
+            [{
+                type: 'UPDATE',
+                condition: new TableStore.Condition(TableStore.RowExistenceExpectation.IGNORE, null),
+                primaryKey: [{ 'gid': Long.fromNumber(8) }, { 'uid': Long.fromNumber(80) }],
+                attributeColumns: [{ 'PUT': [{ 'attrCol1': 'test3' }, { 'attrCol2': 'test4' }] }],
+                returnContent: { returnType: 1 }
+            }]
+        },
+        {
+            tableName: 'sampleTable',
+            rows: [{
+                type: 'PUT',
+                condition: new TableStore.Condition(TableStore.RowExistenceExpectation.IGNORE, null),
+                primaryKey: [{ 'gid': Long.fromNumber(8) }, { 'uid': Long.fromNumber(81) }],
+                attributeColumns: [{ 'attrCol1': 'test1' }, { 'attrCol2': 'test2' }],
+                returnContent: { returnType: TableStore.ReturnType.Primarykey }
+            }]
+        }
+    ],
 };
 
 client.batchWriteRow(params, function (err, data) {
-    
+
     if (err) {
         console.log('error:', err);
         return;
