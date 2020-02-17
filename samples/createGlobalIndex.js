5dcd3bd0bed59bed0161a873d849ffb1f457d321
俟命 <tantan.wtt@alibaba-inc.com>
2020-02-17 18:24:34 +0800

    fix globalIndex includeBaseData param

--- samples/createGlobalIndex.js
+++ samples/createGlobalIndex.js
@@ -4,13 +4,13 @@ var TableStore = require('../index.js');
 client.createIndex({
     mainTableName: "sdkGlobalTest",
     indexMeta: {
-        name: "sdkIndex2",
+        name: "sdkIndex",
         primaryKey: ["col1"],
         definedColumn: ["col2"],
-        includeBaseData: false,
         indexUpdateMode: TableStore.IndexUpdateMode.IUM_ASYNC_INDEX,//默认增量
         indexType: TableStore.IndexType.IT_GLOBAL_INDEX,//IT_GLOBAL_INDEX
-    }
+    },
+    includeBaseData: true
 }, function (err, data) {
     if (err) {
         console.log('error:', err);
