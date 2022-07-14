68483d8420958a719beb1bfd9dad15981d81ceda
俟命 <tantan.wtt@alibaba-inc.com>
2022-07-14 16:41:31 +0800

    5.1.2 fix create global index bug

--- samples/createGlobalIndex.js
+++ samples/createGlobalIndex.js
@@ -3,11 +3,29 @@ var TableStore = require('../index.js');
 
 client.createIndex({
     mainTableName: "sdkGlobalTest",
+    includeBaseData: false,
     indexMeta: {
-        name: "sdkIndex2",
+        name: "noBase",
         primaryKey: ["col1"],
         definedColumn: ["col2"],
-        includeBaseData: false,
+        indexUpdateMode: TableStore.IndexUpdateMode.IUM_ASYNC_INDEX,//默认增量
+        indexType: TableStore.IndexType.IT_GLOBAL_INDEX,//IT_GLOBAL_INDEX
+    }
+}, function (err, data) {
+    if (err) {
+        console.log('error:', err);
+        return;
+    }
+    console.log('success:', JSON.stringify(data, null, 2));
+});
+
+client.createIndex({
+    mainTableName: "sdkGlobalTest",
+    includeBaseData: true,
+    indexMeta: {
+        name: "hasBase",
+        primaryKey: ["col1"],
+        definedColumn: ["col2"],
         indexUpdateMode: TableStore.IndexUpdateMode.IUM_ASYNC_INDEX,//默认增量
         indexType: TableStore.IndexType.IT_GLOBAL_INDEX,//IT_GLOBAL_INDEX
     }
