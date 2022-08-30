f6eb372eb26d6ac5527ccd2cb10432d0486bbc32
xunjian.sl <xunjian.sl@alibaba-inc.com>
2022-08-30 19:06:42 +0800

    refine sql example

--- samples/sql.js
+++ samples/sql.js
@@ -15,6 +15,15 @@ client.sqlQuery(params, function (err, data) {
         for (let i = 0; i < data.sqlRows.rowCount.toFloat64(); i++) {
             for (let j = 0; j < data.sqlRows.columnCount; j++) {
                 let data = data.sqlRows.get(i, j);
+                // 处理binary类型
+                if (resp.sqlRows.sqlTableMeta.schemas[j].typeName === "BINARY") {
+                    let int8Array = data.valueArray();
+                    console.log(int8Array);
+                }
+                // 处理Long类型
+                if (resp.sqlRows.sqlTableMeta.schemas[j].typeName === "LONG") {
+                    console.log(data.toFloat64());
+                }
                 console.log("i:" + i, ", j:" + j + ":" + data);
             }
         }
--- test/sql.test.js
+++ test/sql.test.js
@@ -74,6 +74,15 @@ describe('sql', function () {
             for (let i = 0; i < resp.sqlRows.rowCount.toFloat64(); i++) {
                 for (let j = 0; j < resp.sqlRows.columnCount; j++) {
                     let data = resp.sqlRows.get(i, j);
+                    // 处理binary类型
+                    if (resp.sqlRows.sqlTableMeta.schemas[j].typeName === "BINARY") {
+                        let int8Array = data.valueArray();
+                        console.log(int8Array);
+                    }
+                    // 处理Long类型
+                    if (resp.sqlRows.sqlTableMeta.schemas[j].typeName === "LONG") {
+                        console.log(data.toFloat64());
+                    }
                     console.log("i:" + i, ", j:" + j + ":" + data);
                 }
             }
